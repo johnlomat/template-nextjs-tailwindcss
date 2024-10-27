accc7fb1b803a16436ebbc1e83b8e151a989f8db
John Lomat <edwardlomat1503@gmail.com>
2024-10-27 08:18:40 +0800

    Add: Gray Box component

---
+++ src/components/gray-box.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GrayBox = () => {
+  return (
+    <React.Fragment>
+      <div className="w-full bg-slate-400 p-12"></div>
+    </React.Fragment>
+  )
+}
+
+export default GrayBox
