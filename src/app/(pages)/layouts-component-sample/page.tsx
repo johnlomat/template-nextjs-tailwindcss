9fd14d9c30a8d59a980ffabc29e3313c7832278c
John Lomat <edwardlomat1503@gmail.com>
2024-10-27 08:19:41 +0800

    Change: Gray Box component

--- src/app/(pages)/layouts-component-sample/page.tsx
+++ src/app/(pages)/layouts-component-sample/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Main, Section, Container, Grid, ColSpan } from '@/components/layouts'
+import GrayBox from '@/components/gray-box'
 
 export default function LayoutsCompoenentSample() {
   return (
@@ -20,30 +21,48 @@ export default function LayoutsCompoenentSample() {
           <Container>
             <Grid columns={4}>
               <ColSpan col={4} sm={2} md={1}>
-                <div className="w-full bg-slate-400 p-12"></div>
+                <GrayBox />
               </ColSpan>
               <ColSpan col={4} sm={2} md={1}>
-                <div className="w-full bg-slate-400 p-12"></div>
+                <GrayBox />
               </ColSpan>
               <ColSpan col={4} sm={2} md={1}>
-                <div className="w-full bg-slate-400 p-12"></div>
+                <GrayBox />
               </ColSpan>
               <ColSpan col={4} sm={2} md={1}>
-                <div className="w-full bg-slate-400 p-12"></div>
+                <GrayBox />
               </ColSpan>
             </Grid>
             <Grid columns={4}>
               <ColSpan col={4} sm={2} md={2}>
-                <div className="w-full bg-slate-400 p-12"></div>
+                <GrayBox />
               </ColSpan>
               <ColSpan col={4} sm={2} md={2}>
-                <div className="w-full bg-slate-400 p-12"></div>
+                <GrayBox />
               </ColSpan>
               <ColSpan col={4} sm={2} md={1}>
-                <div className="w-full bg-slate-400 p-12"></div>
+                <GrayBox />
               </ColSpan>
               <ColSpan col={4} sm={2} md={1}>
-                <div className="w-full bg-slate-400 p-12"></div>
+                <GrayBox />
+              </ColSpan>
+            </Grid>
+          </Container>
+        </Section>
+        <Section className="pt-0" rowSpacing="md">
+          <Container>
+            <Grid>
+              <ColSpan>
+                <div className="text-2xl font-bold">
+                  <h1>Fullwidth container</h1>
+                </div>
+              </ColSpan>
+            </Grid>
+          </Container>
+          <Container fullWidth>
+            <Grid>
+              <ColSpan>
+                <GrayBox />
               </ColSpan>
             </Grid>
           </Container>
